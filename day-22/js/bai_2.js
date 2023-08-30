0103addba61a9aa3904687310db0fa46d17530c2
Miu <duchainguyen134@gmail.com>
2023-08-30 16:34:25 +0700

    day-22

--- day-22/js/bai_2.js
+++ day-22/js/bai_2.js
@@ -10,6 +10,7 @@ Array.prototype.reduce2 = function (callback, value) {
   for (var i = value === undefined ? 1 : 0; i < this.length; i++) {
     // var result = callback(this[i-1], this[i], i);
     var result = callback(result, this[i], i);
+    // console.log(add);
     // console.log(result);
   }
   return result;
@@ -17,6 +18,7 @@ Array.prototype.reduce2 = function (callback, value) {
 // console.log(number.reduce2());
 var result = number.reduce2(function (prew, current, index) {
   console.log(`p= ${prew}, c = ${current}, i = ${index} `);
+  // return prew + current;
   return current;
 }, 0);
 
@@ -28,3 +30,28 @@ console.log(result);
 // },0);
 
 // console.log(result);
+
+// bài tập
+var students = ["An", "Dương", "Vương", "Hải", "Khang"];
+
+var studentsActive = ["Dương", "Hải", "Vương"];
+
+// An Khang
+
+Array.prototype.reduce2 = function (callback, value) {
+  var result = value !== undefined ? value : this[0];
+  for (var i = value === undefined ? 1 : 0; i < this.length; i++) {
+    var result = callback(result, this[i], i);
+  }
+  return result;
+};
+var result = students.reduce2(function (prev, current) {
+  // var newArr = [];
+  // console.log(prev, current);
+  if (!studentsActive.includes(current)) {
+    prev.push(current);
+  }
+  return prev;
+}, []);
+
+console.log(result);
